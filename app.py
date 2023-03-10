b7293e7cfa161af6eb95c5895892040ed9df3244
Mykola Murashko <mykola.murashko@wagepoint.com>
2023-03-10 15:01:02 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,133 @@
+# Start -menu 
+# 1) Registration
+# 2) Auth
+# 3) Product catalog
+# 4) Buy product
+# 5) Money
+# 6) Exit
+####################################################
+
+# def registration (user_name , password, money ) :
+#    return {
+#         "user_name" : user_name,
+#         "password" : password,
+#         "money" : money
+#    }
+
+# def auth ( user_name, password, registered_user) :
+#     for item in registered_user:
+#         if item['user_name']==user_name and item['password']==password :
+#             return True
+
+
+# def show_products_catalog(list_of_products_catalog) :
+#     for products_catalog_item in list_of_products_catalog :
+#         print(products_catalog_item)
+
+# def money_user (money, data_base) :
+#     if money_user != 0:
+#         print("You can continue shopping") 
+#     else :
+#         print("You pocket is empty")
+
+
+# products_catalog = [
+#     {
+#         "label" : "Lavazza",
+#         "product" : "coffee",
+#         "price" : "500 uah"
+#     },
+#     {
+#         "label" : "Ambassador",
+#         "product" : "coffee",
+#         "price" : "600 uah"
+#     },
+#     {
+#         "label" : "Tchibo",
+#         "product" : "coffee",
+#         "price" : "400 uah"
+#     },
+#     {
+#         "label" : "Raffaello",
+#         "product" : "candy",
+#         "price" : "300 uah"
+#     },
+#     {
+#         "label" : "Ritter sport",
+#         "product" : "candy",
+#         "price" : "300 uah"
+#     },
+#     {
+#         "label" : "Lind Lindor",
+#         "product" : "candy",
+#         "price" : "400 uah"
+#     }
+# ]
+
+
+# data_base = [
+#     {
+#         "user_name" : "Milan",
+#         "password" : "123456",
+#         "money" : "2000"
+#     },
+#     {
+#         "user_name" : "Katrin",
+#         "password" : "98765",
+#         "money" : "1500"
+#     },
+#     {
+#         "user_name" : "Milan",
+#         "password" : "123456",
+#         "money" : "3000"
+#     },
+# ]
+
+
+# is_running = True
+
+# is_has_account = False
+
+# while is_running :
+#     choose_action = input("""
+#         1) Registration
+#         2) Auth
+#         3) Product catalog
+#         4) Buy product
+#         5) Exit
+#     Answer : """.lower()    
+#     ) 
+
+#     if choose_action == "3" :
+#        show_products_catalog(products_catalog)
+
+
+
+#     elif choose_action == "1" :
+#          user_name = input("Enter your name : " )
+#          password = input("Enter your password  : ")
+#          money = input("How much money do you have ?  : ")
+
+
+
+
+  
+
+
+
+
+  
+    
+
+    
+
+   
+
+
+
+        
+
+ 
+
+
+     
